3ba2e2779fb5316350b85f5991e465f74b994f35
Dimon <notawid@gmail.com>
2024-07-18 17:45:55 +0200

    Profile tab login window mobile view styling

--- src/components/CoNexus/profile.svelte
+++ src/components/CoNexus/profile.svelte
@@ -1,6 +1,28 @@
 <script>
   export let user;
   export let codes;
+
+  let isLogged = false;
+  let walletConnected = false;
+  let walletAddress;
+
+  function log() {
+    if(isLogged) {
+      isLogged = false;
+    } else {
+      checkValidity();
+      isLogged = true;
+    }
+  }
+
+  function connectWallet() {
+    if(walletConnected) {
+      walletConnected = false;
+    } else {
+      walletConnected = true;
+      walletAddress = '0xeb0a...60c1';
+    }
+  }
 </script>
 
 
@@ -10,46 +32,114 @@
   <section class="user-profile">
     <div class="log-in">
       <button class="close-button">Close</button>
-      <button class="login-button">Log out</button>
+      {#if isLogged}
+        <button class="login-button" on:click={log}>Log out</button>
+      {:else if !isLogged}
+        <button class="how-button">How to sign up?</button>
+      {/if}
     </div>
 
     <hr>
 
-    <div class="user-profile-info">
-      <div class="user-property">
-        <p class="user-prop">Mail:</p>
-        <p class="user-prop">Password:</p>
-        <p class="user-prop">First name:</p>
-        <p class="user-prop">Last name:</p>
+    {#if isLogged}
+
+      <div class="user-profile-info">
+        <div class="user-property">
+          <p class="user-prop">Mail:</p>
+          <p class="user-prop">Password:</p>
+          <p class="user-prop">First name:</p>
+          <p class="user-prop">Last name:</p>
+        </div>
+        <div class="property-value">
+          <p class="user-prop-value mail">{user.mail}</p>
+          <p class="user-prop-value password">{user.password}</p>
+          <p class="user-prop-value first-name">{user.first_name}</p>
+          <p class="user-prop-value last-name">{user.last_name}</p>
+        </div>
       </div>
-      <div class="property-value">
-        <p class="user-prop-value mail">{user.mail}</p>
-        <p class="user-prop-value password">{user.password}</p>
-        <p class="user-prop-value first-name">{user.first_name}</p>
-        <p class="user-prop-value last-name">{user.last_name}</p>
+
+      <hr>
+
+      <div class="wallet-connect">
+        <p class="user-prop">Your wallet:</p>
+
+        <button class="wallet-button" on:click={connectWallet}>
+          {#if !walletConnected}
+            Connect wallet
+          {:else if walletConnected}
+            {walletAddress}
+          {/if}
+        </button>
+        
       </div>
-    </div>
 
-    <hr>
+      <hr>
 
-    <div class="wallet-connect">
-      <p class="user-prop">Your wallet:</p>
-      <button class="wallet-button">Connect wallet</button>
-    </div>
+      <p class="refferal-codes-legend">Your refferal codes</p>
+      <div class="refferal-codes">
+        {#each codes as code}
+          <p class="ref-code used-{code.is_used}">{code.code}</p>
+        {/each}
+      </div>
 
-    <hr>
+    {:else if !isLogged}
 
-    <p class="refferal-codes-legend">Your refferal codes</p>
-    <div class="refferal-codes">
-      {#each codes as code}
-        <p class="ref-code used-{code.is_used}">{code.code}</p>
-      {/each}
-    </div>
+      <form class="login-form">
+        <label class="input-label" for="user-mail">Mail</label>
+        <input class="user-input" type="email" id="user-mail" placeholder="Enter your email" required>
+        <label class="input-label" for="user-password">Password</label>
+        <input class="user-input" type="password" id="user-password" placeholder="Enter your password" minlength="8" required>
+        <button class="submit-button" on:click={log}>Log-in</button>
+      </form>
+
+      <hr>
+
+      <form class="signup-form">
+        <input class="user-input" type="text" id="refferal-code" placeholder="Enter your refferal code" minlength="16" required>
+        <button class="submit-button">Sign-up</button>
+      </form>
+    {/if}
+
   </section>
 </div>
 
 
 <style>
+  .login-form, .signup-form {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .input-label {
+    font-size: 1.5vw;
+    line-height: 1.5vw;
+    margin-bottom: 0.5vw;
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  .user-input {
+    width: 30vw;
+    font-size: 2vw;
+    line-height: 2.5vw;
+    padding: 2vw;
+    margin-bottom: 2vw;
+    color: rgba(51, 226, 230, 0.75);
+    border: 0.1vw solid rgba(51, 226, 230, 0.5);
+    border-radius: 2.5vw;
+    background-color: rgba(1, 0, 32, 0.75);
+    outline: none;
+  }
+
+  .submit-button {
+    width: 30vw;
+    padding: 1vw;
+    color: rgba(51, 226, 230, 0.75);
+    background-color: rgba(51, 226, 230, 0.1);
+    cursor: pointer;
+  }
+
   hr {
     margin: 2vw 0;
     border: 0.1vw solid rgba(51, 226, 230, 0.5);
@@ -166,8 +256,25 @@
 
 
   @media only screen and (max-width: 600px) {
+    .input-label {
+      font-size: 0.9em;
+      line-height: 0.9em;
+      padding-bottom: 0.5em;
+    }
+
+    .user-input {
+      width: 80vw;
+      font-size: 1.4em;
+      line-height: 1.6em;
+      margin-bottom: 1em;
+    }
+
+    .submit-button {
+      width: 50vw;
+    }
+
     hr {
-      margin: 1em 0;
+      margin: 2em 0;
     }
 
     .user-profile {
@@ -176,6 +283,7 @@
       left: 0;
       right: 0;
       padding-top: 5%;
+      padding-bottom: 5%;
       border-radius: 0;
       background-color: rgba(1, 0, 32, 0.85);
     }
