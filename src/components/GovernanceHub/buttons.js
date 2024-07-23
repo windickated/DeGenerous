fa223ea05d5eaf5fc8c7afba325ac040ee7659bd
Dimon <notawid@gmail.com>
2024-07-23 10:56:36 +0200

    Created Governance Hub page and imported display & console components

---
+++ src/components/GovernanceHub/buttons.js
@@ -0,0 +1,54 @@
+export const displayButtons = [
+  {
+    id: "switcher",
+    video: "/public/governanceHubAssets/video.png",
+    text: "/public/governanceHubAssets/text.png",
+    videoHover: "/public/governanceHubAssets/video-hover.png",
+    textHover: "/public/governanceHubAssets/text-hover.png"
+  },
+  {
+    id: "vote",
+    image: "/public/governanceHubAssets/vote-clickable.png",
+    hover: "/public/governanceHubAssets/vote-hover.png",
+    click: "/public/governanceHubAssets/vote-active.png",
+    inactive: "/public/governanceHubAssets/vote-inert.png"
+  }
+]
+
+export const consoleButtons = [
+  {
+    id: "conexus",
+    image: "/public/governanceHubAssets/conexus.png",
+    hover: "/public/governanceHubAssets/conexus-hover.png",
+    click: "/public/governanceHubAssets/conexus-active.png",
+    class: "console-btn big"
+  },
+  {
+    id: "back",
+    image: "/public/governanceHubAssets/back.png",
+    hover: "/public/governanceHubAssets/back-hover.png",
+    click: "/public/governanceHubAssets/back-active.png",
+    class: "console-btn small"
+  },
+  {
+    id: "omnihub",
+    image: "/public/governanceHubAssets/omnihub.png",
+    hover: "/public/governanceHubAssets/omnihub-hover.png",
+    click: "/public/governanceHubAssets/omnihub-active.png",
+    class: "console-btn big"
+  },
+  {
+    id: "forward",
+    image: "/public/governanceHubAssets/forward.png",
+    hover: "/public/governanceHubAssets/forward-hover.png",
+    click: "/public/governanceHubAssets/forward-active.png",
+    class: "console-btn small"
+  },
+  {
+    id: "sagaverse",
+    image: "/public/governanceHubAssets/sagaverse.png",
+    hover: "/public/governanceHubAssets/sagaverse-hover.png",
+    click: "/public/governanceHubAssets/sagaverse-active.png",
+    class: "console-btn big"
+  }
+]
